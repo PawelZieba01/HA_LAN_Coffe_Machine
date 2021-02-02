425ceceeed0e45d96ccb22de162e6f862bf22514
Paweł <zpawel.zieba@gmail.com>
2021-02-02 01:01:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from machine import Pin, PWM, deepsleep
+from machine import Pin
 import urequests
 import network
 import socket
 import ujson
 import gc
-#import select
-from time import sleep
+import _thread
+from time import sleep, time
 
 gc.collect()
 
@@ -29,50 +29,46 @@ server_port = config_dict["server_port"]
 
 api_token = config_dict["api_token"]
 
-#device1_idx = config_dict["device1_idx"]
-#device2_idx = config_dict["device2_idx"]
-
-#relay1_pin = int(config_dict["relay1_pin"])
-#relay2_pin = int(config_dict["relay2_pin"])
-
-#request_period = float(config_dict["request_period"])
-#relay_pins_invert = config_dict["relay_pins_invert"]
 cmd_start = config_dict["cmd_start"]
-#cmd_off = config_dict["cmd_off"]
 
 del config_dict
 #------------------------------------------------------------------------------------------------------------
 
 
 
-#-------------------------------------------   ZMIENNE GLOBALNE   -------------------------------------------
-
-#------------------------------------------------------------------------------------------------------------
-
-
 
 #------------------------------------------   KONFIGURACJA PINÓW   ------------------------------------------
 led = Pin(2, Pin.OUT)    #wbudowana dioda led
 
-#relay1 = Pin(relay1_pin, Pin.OUT)    #przekaźnik 1
-#relay2 = Pin(relay2_pin, Pin.OUT)    #przekaźnik 2
+#piny czytające stany diod led
+led1_pin = Pin(34, Pin.IN)
+led2_pin = Pin(35, Pin.IN)
+led3_pin = Pin(33, Pin.IN)
+led4_pin = Pin(32, Pin.IN)
+
+head_open_sw_pin = Pin(25, Pin.IN)    #czujnik otwarcia głowicy
+
+head_sw_pin = Pin(4, Pin.OUT)    #wyjście rozłączające styk zamknięcia głowicy, 0 - obwód otwarty, 1 - obwód zamkniety
+el_sw_pin = Pin(26, Pin.OUT)     #wyjście załączające ekspres
 
 
-#wyłączenie przekaźników
-# if(relay_pins_invert == "False"):
-#     relay1.value(0)
-#     relay2.value(0)
-# else:
-#     relay1.value(1)
-#    relay2.value(1)
+##
+head_sw_pin.value(1)             #obwód czujnika otwarcia głowicy zamknięty
+##
+#------------------------------------------------------------------------------------------------------------
 
+
+
+#-------------------------------------------   ZMIENNE GLOBALNE   -------------------------------------------
+last_led_pin_values = [led1_pin.value(), led2_pin.value(), led3_pin.value(), led4_pin.value()]       #poprzedni stan diod
+reset_tslcp_times = [0,0,0,0]                                                                        #czasy w których zresetowano zmienne 'time_since_last_change_pins'
+time_since_last_change_pins = [0,0,0,0]
 #------------------------------------------------------------------------------------------------------------
 
 
 
 #-----------------------------------   KONFIGURACJA POŁĄCZEŃ SIECIOWYCH   -----------------------------------
 #utworzenie socketu do komunikacji - client
-#s_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #inicjalizacja WLAN
@@ -121,35 +117,222 @@ s_server.listen(5)
 #-----------------------------------------------   FUNKCJE   ------------------------------------------------
 
 #funkcja pobierająca i konwertująca dane z serwera domoticz
-def send_data_to_HA():
+def send_data_to_HA(data):
     global server_ip, server_port
     
     url = 'http://' + server_ip + ':' + server_port + '/api/states/sensor.ekspres_do_kawy'
     headers = {"Authorization": "Bearer " + api_token}
-    data = '{"state":"Robimy kawe ;)"}'
+    #data = '{"state":"Robimy kawe ;)"}'
     
-    response = urequests.post(url, headers=headers, data=data)     #wysłanie danych do serwera - POST
-    print(response)
+    res = urequests.post(url, headers=headers, data=data)     #wysłanie danych do serwera - POST
+    res.close()
+    print(res)
+    print(data)
+            
+            
+            
+            
+#funkcja pobierająca czasy od ostatnich zmian stanów diod
+def get_leds_time():
+    global time_since_last_change_pins, reset_tslcp_times, last_led_pin_values
+    
+    current_pin_values = [led1_pin.value(), led2_pin.value(), led3_pin.value(), led4_pin.value()]
+    for i in range(4):
+        time_since_last_change_pins[i] = time() - reset_tslcp_times[i]
+
+        if(last_led_pin_values[i] != current_pin_values[i]):
+            #print("zmiana stanu" + str(i))
+            last_led_pin_values[i] = current_pin_values[i]
+            reset_tslcp_times[i] = time()
+            time_since_last_change_pins[i] = 0
+
+
+
+
+#funkcja pobierająca informacje w jakim trybie jest ekspres
+def get_express_status():
+    global time_since_last_change_pins, reset_tslcp_times, last_led_pin_values
+    
+    #ekspres gotowy do pracy w trybia 'auto'
+    if(led1_pin.value() == 0 and time_since_last_change_pins[0] > 2 and time_since_last_change_pins[1] > 2 and time_since_last_change_pins[2] > 2 and time_since_last_change_pins[3] > 2):
+        print("TRYB 1: gotowy do pracy")
+        return 1
+        
+    #ekspres w trakcie pracy - tryb 'auto'
+    elif(time_since_last_change_pins[0] < 2 and time_since_last_change_pins[1] > 2 and time_since_last_change_pins[2] > 2 and time_since_last_change_pins[3] > 2):
+        print("TRYB 2: kawa w trakcie przygotowywania")
+        return 2
+    
+    #ekspres zakończył pracę - jest w trybie 'manual'
+    elif(led2_pin.value() == 0 and time_since_last_change_pins[0] > 2 and time_since_last_change_pins[1] > 2 and time_since_last_change_pins[2] > 2 and time_since_last_change_pins[3] > 2):
+        print("TRYB 3: kawa gotowa!")
+        return 3
+        
+    #ekspres ma zagrzaną wodę ale nie ogarnia żeby się włączyć przyciskiem - trzeba zasymulować otwarcie i zamknięcie głowicy
+    elif(led3_pin.value() == 0 and time_since_last_change_pins[0] > 2 and time_since_last_change_pins[1] > 2 and time_since_last_change_pins[2] > 2 and time_since_last_change_pins[3] > 2):
+        print("TRYB 4: woda jest ciepła - otwórz i zamknij głowicę")
+        return 4
+        
+    #ekspres w trybie standby
+    elif(time_since_last_change_pins[0] > 2 and time_since_last_change_pins[1] > 2 and time_since_last_change_pins[2] < 2 and time_since_last_change_pins[3] > 2):
+        print("TRYB 5: grzanie wody")
+        return 5
+        
+    #ekspres w trybie standby
+    elif(led4_pin.value() == 0 and time_since_last_change_pins[0] > 2 and time_since_last_change_pins[1] > 2 and time_since_last_change_pins[2] > 2 and time_since_last_change_pins[3] > 2):
+        print("TRYB 6: standby")
+        return 6
+        
+    #brak wody lub kapsułki
+    elif(time_since_last_change_pins[0] < 2 and time_since_last_change_pins[1] < 2 and time_since_last_change_pins[2] < 2 and time_since_last_change_pins[3] < 2):
+        print("BŁĄD: brak wody lub kapsułki")
+        return 0
+    else:
+        return -1
+
+
+#funkcja zwracająca dane do wyświetlenia na interfejsie graficznym w zależności od statusu ekspresu
+def get_data_to_send_to_HA(status):
+    
+    if(status == 0):
+        data = b'{"state":"Brak wody lub kapsułki :( "}'
+    elif(status == 1 or status == 4 or status == 6):
+        data = b'{"state":"Kawy? "}'
+    elif (status == 2):
+        data = b'{"state":"Robimy kawę!!!"}'
+    elif (status == 3):
+        data = b'{"state":"Kawa gotowa!!!"}'
+    elif(status == 5):
+        data = b'{"state":"Grzejemy wode ;)"}'
+    else:
+        data = data = b'{"state":"status -1"}'
+    
+    return data
+    
+    
+
+
+#funkcja sprawdzająca czy głowica została otwarta i zamknięta, 0 - zamknięta, 1 - otwarta
+def get_head_status():
+    global last_head_status
+    
+    current_head_status = head_open_sw_pin.value()
+    if(last_head_status == 0 and current_head_status == 1):
+        print('otworzono głowice')
+        last_head_status = current_head_status
+        
+    elif(last_head_status == 1 and current_head_status == 0):
+        print('zamknięto głowice')
+        last_head_status = current_head_status
+
+
+
+#funkcja odpowiedzialna za robienie kawy
+def make_coffee():
+    status = get_express_status()
+    
+    if(status == 1):
+        el_sw_pin.value(1)
+        sleep(0.2)
+        el_sw_pin.value(0)
+        
+    elif(status == 2 or status == 3 or status == 0):
+        return 0
+    
+    elif(status == 4):
+        
+        head_sw_pin.value(0)
+        sleep(0.5)
+        head_sw_pin.value(1)
+        
+        sleep(4)
+        el_sw_pin.value(1)
+        sleep(0.2)
+        el_sw_pin.value(0)
+        
+    elif(status == 5):
+        while(status != 1 and status != 4):
+            pass
+        if(status == 1):
+            el_sw_pin.value(1)
+            sleep(0.1)
+            el_sw_pin.value(0)
+        elif(status == 4):
+            head_sw_pin.value(0)
+            sleep(0.5)
+            head_sw_pin.value(1)
+            
+            sleep(2)
+            el_sw_pin.value(1)
+            sleep(0.1)
+            el_sw_pin.value(0)
+            
+    elif(status == 6):
+        while(status != 1 and status != 4):
+            pass
+        if(status == 1):
+            el_sw_pin.value(1)
+            sleep(0.1)
+            el_sw_pin.value(0)
+        elif(status == 4):
+            head_sw_pin.value(0)
+            sleep(0.5)
+            head_sw_pin.value(1)
+            
+            sleep(2)
+            el_sw_pin.value(1)
+            sleep(0.1)
+            el_sw_pin.value(0)
+        
+#------------------------------------------------------------------------------------------------------------
+
+
+
+#--------------------------------   WĄTEK OBSŁUGUJĄCY POBIERANIE I AKTUALIZACJĘ INFORMACJI O EKSPRESIE   --------------------------------
+def express_status_thread():
+    print('wątek wystartował!')
+    
+    #wątek obsługujący komunikację sieciową
+    while True:
+        conn, addr = s_server.accept()
+        print('Odebrano połączenie z %s' % str(addr))
+        request = conn.recv(1024)
+        conn.send('HTTP/1.1 200 OK\n')
+        conn.send('Content-Type: text/html\n')
+        conn.send('Connection: close\n\n')
+        conn.close()
+        #print('Wiadomość = %s' % str(request))
+
+        #jeśli odebrano rozkaz robienia kawy
+        if(str(request).find(cmd_start) != -1):
+            print("robimy kawe")
+            make_coffee()
+    
+    
+
 
 #------------------------------------------------------------------------------------------------------------
 
 
 
-
-
-
 #----------------------------------------   GŁÓWNA PĘTLA PROGRAMU   -----------------------------------------
+_thread.start_new_thread(express_status_thread, ())       #uruchomienie wątku express_status_thread()
+
+last_data_to_send = b'{"state":"Kawy? "}'
+send_data_to_HA(last_data_to_send)             #wstępne wysłanie danych do serwera
+express_status = 0
+
+
 while True:
-    conn, addr = s_server.accept()
-    print('Odebrano połączenie z %s' % str(addr))
-    request = conn.recv(1024)
-    conn.send('HTTP/1.1 200 OK\n')
-    conn.send('Content-Type: text/html\n')
-    conn.send('Connection: close\n\n')
-    conn.close()
-    #print('Wiadomość = %s' % str(request))
+    get_leds_time()
+    express_status = get_express_status()                          #pobranie statusu ekspresu
+    data_to_send = get_data_to_send_to_HA(express_status)          #przygotowanie danych do wysłania na podstawie statusu
     
-    if(str(request).find(cmd_start) != -1):
-        print("robimy kawe")
-        send_data_to_HA()
+    print(data_to_send)
+    #jeżeli zmienił się status i poprawnie go odczytano 
+    if(last_data_to_send != data_to_send and express_status != -1):
+        last_data_to_send = data_to_send
+        send_data_to_HA(data_to_send)                              #wysłanie danych do serwera
+    
+    sleep(0.1)
 #------------------------------------------------------------------------------------------------------------    
