02d07aa36cadee45fc962b86e2b1cd9a302065dc
Paweł <zpawel.zieba@gmail.com>
2021-03-29 12:59:26 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,21 +1,21 @@
-def do_connect(ssid, pwd):
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.ifconfig(("192.168.1.41", "255.255.255.0", "192.168.1.1", "8.8.8.8"))
-        sta_if.connect(ssid, pwd)
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
- 
-# This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
- 
-# Attempt to connect to WiFi network
-do_connect('bestconnect.pl 130', 'pawel130')
- 
-import webrepl
-webrepl.start()
+# def do_connect(ssid, pwd):
+#     import network
+#     sta_if = network.WLAN(network.STA_IF)
+#     if not sta_if.isconnected():
+#         print('connecting to network...')
+#         sta_if.active(True)
+#         sta_if.ifconfig(("192.168.1.41", "255.255.255.0", "192.168.1.1", "8.8.8.8"))
+#         sta_if.connect(ssid, pwd)
+#         while not sta_if.isconnected():
+#             pass
+#     print('network config:', sta_if.ifconfig())
+#  
+# # This file is executed on every boot (including wake-boot from deepsleep)
+# #import esp
+# #esp.osdebug(None)
+#  
+# # Attempt to connect to WiFi network
+# do_connect('bestconnect.pl 130', 'pawel130')
+#  
+# import webrepl
+# webrepl.start()
